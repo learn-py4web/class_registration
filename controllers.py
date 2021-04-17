126a9bb71e8cd2d9151bc9724721044b3b4c0c4d
Luca de Alfaro <luca@dealfaro.org>
2021-04-16 19:09:44 -0700

    After query

--- controllers.py
+++ controllers.py
@@ -25,6 +25,44 @@ session, db, T, auth, and tempates are examples of Fixtures.
 Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app will result in undefined behavior
 """
 
+"""
+    Query examples: 
+    
+    # Catalog description of CSE 183?
+    db(db.catalog_class.number == "CSE 183").select()
+
+    # Offerings of CSE 183 in Spring 2021?
+    db(
+        (db.catalog_class.number == "CSE 183") &
+        (db.quarter.year == 2021) & (db.quarter.name == "Spring") &
+        # This encodes class offering ---> catalog class
+        (db.class_offering.catalog_class_id == db.catalog_class.id) &
+        # This encodes class offering ---> quarter
+        (db.class_offering.quarter_id == db.quarter.id)
+    ).select()
+
+    # Who are the students on the waitlist for CSE 183 Spring 2021?
+    rows = db(
+        (db.catalog_class.number == "CSE 183") &
+        (db.quarter.year == 2021) & (db.quarter.name == "Spring") &
+        # This encodes class offering ---> catalog class
+        (db.class_offering.catalog_class_id == db.catalog_class.id) &
+        # This encodes class offering ---> quarter
+        (db.class_offering.quarter_id == db.quarter.id) &
+        # This encodes registration ---> offering
+        (db.registration.class_offering_id == db.class_offering.id) &
+        # This encodes registration ---> student
+        (db.registration.student_id == db.student.id) &
+        # Oh, and the registration has to be on the wait list.
+        (db.registration.is_waitlist == True)
+    ).select()
+    for row in rows:
+        print(row.student.first_name, row.student.last_name,
+              "is on the waitlist")
+
+"""
+
+
 from py4web import action, request, abort, redirect, URL
 from yatl.helpers import A
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
@@ -36,5 +74,5 @@ url_signer = URLSigner(session)
 @action('index')
 @action.uses(db, auth, 'index.html')
 def index():
-    print("User:", get_user_email())
+
     return dict()
--- models.py
+++ models.py
@@ -6,18 +6,63 @@ import datetime
 from .common import db, Field, auth
 from pydal.validators import *
 
-
 def get_user_email():
     return auth.current_user.get('email') if auth.current_user else None
 
 def get_time():
     return datetime.datetime.utcnow()
 
-
 ### Define your table below
 #
 # db.define_table('thing', Field('name'))
 #
 ## always commit your models to avoid problems later
+
+db.define_table(
+    'catalog_class', # This table, like all tables, will have an "id"
+    Field('number'), # CSE 183
+    Field('name'), # Web apps
+    Field('description', 'text'),
+)
+
+db.define_table(
+    'quarter',
+    Field('year', 'integer'),
+    Field('season'), # Winter
+)
+
+db.define_table(
+    'class_offering',
+    Field('catalog_class_id', 'reference catalog_class'),
+    Field('quarter_id', 'reference quarter', ondelete="CASCADE"),
+    Field('number', 'integer'),
+    Field('taught_by', 'reference instructor'),
+    Field('active', 'boolean') # Will this take place?
+)
+
+db.define_table(
+    'student',
+    Field('email'),
+    Field('first_name'),
+    Field('last_name'),
+    Field('suid'),
+)
+
+db.define_table(
+    'registration',
+    Field('student_id', 'reference student'),
+    Field('class_offering_id', 'reference class_offering'),
+    Field('is_waitlist', 'boolean'),
+    Field('waitlist_pos', 'integer'),
+    Field('note', 'text'),
+    Field('registration_date', 'datetime'),
+)
+
+db.define_table(
+    'instructor',
+    Field('email'),
+    Field('first_name'),
+    Field('last_name'),
+)
 
 db.commit()
